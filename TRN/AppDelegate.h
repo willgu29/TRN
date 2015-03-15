5a51ea0b586c95f1059d98635926d66007a353db
Will Gu <willgu29@gmail.com>
2015-03-15 00:24:32 -0700

    First Commit

---
+++ TRN/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TRN
+//
+//  Created by William Gu on 3/15/15.
+//  Copyright (c) 2015 William Gu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
